073707dc69a3d9666afd46ea12dfe435ec91d34b
Momaxguy <95580747+Momaxguy@users.noreply.github.com>
2022-10-07 11:18:29 -0400

    Initial commit

---
+++ src/main/java/net/momax/sniffer/Sniffer.java
@@ -0,0 +1,16 @@
+package net.momax.sniffer;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sniffer implements ModInitializer {
+	public static final String MOD_ID = "sniffer";
+	public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
